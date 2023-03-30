49111f52c60d6d9487413c948376f4b283dc92bf
Vishvajit Gadakari <gadakariv@gmail.com>
2023-03-31 00:36:40 +0530

    Updates

---
+++ precache-manifest.3a332ece5ebbd67f2c21de58e4357fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b7f17065baef6dace2bd38db6b3d82",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "1c26282f7596987b0b5f",
+    "url": "/My-Portfolio/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "267225d335eee8e1efad",
+    "url": "/My-Portfolio/static/css/main.2dd219b4.chunk.css"
+  },
+  {
+    "revision": "1c26282f7596987b0b5f",
+    "url": "/My-Portfolio/static/js/2.912f4d8d.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/My-Portfolio/static/js/2.912f4d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267225d335eee8e1efad",
+    "url": "/My-Portfolio/static/js/main.2102e514.chunk.js"
+  },
+  {
+    "revision": "3497a26e3282d0edebf8",
+    "url": "/My-Portfolio/static/js/runtime-main.4278d206.js"
+  },
+  {
+    "revision": "017d8307caaa3cb1a555db447fcf49a1",
+    "url": "/My-Portfolio/static/media/sale.017d8307.png"
+  }
+]);
